137e19d57e2d0f11ff17f9cb584d217e0c363b17
Alex Chien <alex.chien@koocaa.com>
2020-03-04 13:00:39 +0800

    update baseUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,15 +21,15 @@ const users = [{
 const siteConfig = {
   title: 'Darwinia Documentation', // Title for your website.
   tagline: 'Darwinia Documentation',
-  url: 'https://docs.darwinia.network', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://alexchien.git', // Your website URL
+  baseUrl: '/docusaorus', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'darwinia-docs',
-  organizationName: 'darwinia',
+  projectName: 'docusaorus',
+  organizationName: 'AlexChien',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
