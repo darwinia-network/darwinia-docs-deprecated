e9bcf6582fa5c7e23501c114462452ec4af9850a
Alex Chien <alex.chien@koocaa.com>
2020-03-25 04:25:22 +0800

    header link

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,7 +39,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { blog: true, label: 'Blog' },
-    { doc: 'wiki-home', label: 'Wiki' },
+    { doc: 'wiki-intro', label: 'Wiki' },
     { doc: 'crab-home', label: 'Crab Network' },
     { doc: 'dev-home', label: 'Developers' },
     // { doc: 'doc4', label: 'API' },
