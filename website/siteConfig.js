81eeb7a6f3b5a8ae97d9f66ad6cd0a60e1858f9e
Aki Wu <wuminzhe@gmail.com>
2021-03-22 13:45:54 +0800

    Change link name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -41,7 +41,7 @@ const siteConfig = {
     { href: "https://darwinia.network/", label: "Home", external: true },
     { blog: true, label: 'Blog' },
     { doc: 'wiki-intro', label: 'Docs' },
-    { href: "https://crab.network/", label: "Crab Home", external: true },
+    { href: "https://crab.network/", label: "Crab Network", external: true },
     { doc: 'dev-home', label: 'Developers' },
     // { doc: 'doc4', label: 'API' },
     // { page: 'help', label: 'Help' },
