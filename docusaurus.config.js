55394ab99fa347012431a6893463abffa5b34b6c
JayJay1024 <lygm1062@gmail.com>
2021-10-07 22:13:22 +0800

    docs: footer copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Darwinia Network. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
